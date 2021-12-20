2ec623ddfd0905dd313ae9cc2469a4160e2f4e7b
unknown <alexsitalo1983@gmail.com>
2021-12-20 19:04:27 +0200

    HomeWork Class 30

---
+++ src/com/company/class30/HW/Task1.java
@@ -0,0 +1,38 @@
+package com.company.class30.HW;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+//Create a property file to store following configurations:
+//browser=chrome
+//url=https://facebook.com
+//username
+//password
+//Read file and extract values of browser & url
+public class Task1 {
+    public static void main(String[] args) throws IOException {
+        String path="Files/Facebook.properties";
+        Properties properties=new Properties();
+        properties.put("username","Alex");
+        properties.put("password","pas123");
+        properties.put("browser","chrome");
+        properties.put("url","https://facebook.com");
+
+        FileOutputStream fileOutputStream=new FileOutputStream(path);
+        properties.store(fileOutputStream, "Creating a HomeWork file");
+
+        System.out.println("The browser we are using: "+properties.get("browser"));
+        System.out.println("URL is: "+properties.get("url"));
+
+
+
+
+
+
+    }
+
+}
